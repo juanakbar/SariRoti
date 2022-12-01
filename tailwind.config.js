c403bd91b0f9e44ed966c54db1341acee2893589
juanakbar <juanindrian02@gmail.com>
2022-12-02 03:32:57 +0700

    Kau Jaga Adek Aku

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './src/**/*.{html,js}',
+        './public/**/*.{html,js}',
+        './node_modules/flowbite/**/*.js',
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            container: {
+                center: true,
+                padding: '1rem',
+            },
+            colors: {
+                primary: {
+                    50: '#eff6ff',
+                    100: '#dbeafe',
+                    200: '#bfdbfe',
+                    300: '#93c5fd',
+                    400: '#60a5fa',
+                    500: '#3b82f6',
+                    600: '#2563eb',
+                    700: '#1d4ed8',
+                    800: '#1e40af',
+                    900: '#1e3a8a',
+                },
+            },
+        },
+        fontFamily: {
+            sans: ['Poppins'],
+        },
+    },
+    plugins: [require('flowbite/plugin'), require('@tailwindcss/aspect-ratio')],
+}
